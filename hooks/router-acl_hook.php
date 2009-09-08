5f1be91644fc6ec295477875b646cfaad6d2f178
Zeelot <Zeelot3k@gmail.com>
2009-09-07 22:19:02 -0700

    added the hook as found in yurikocms 0.1.2

---
+++ hooks/router-acl_hook.php
@@ -0,0 +1,59 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @package    YurikoCMS
+ * @author     Lorenzo Pisani - Zeelot
+ * @copyright  (c) 2008-2009 Lorenzo Pisani
+ * @license    http://yurikocms.com/license
+ */
+
+class simple_acl_hook {
+
+	protected $user_roles = array();
+
+	public function __construct()
+	{
+		if($user = Session::instance()->get('auth_user', FALSE))
+		{
+			foreach($user->roles as $role)
+			{
+				$this->user_roles[] = $role->name;
+			}
+		}
+		if (count($this->user_roles) < 1)
+		{
+			$this->user_roles[] = 'guest';
+		}
+		Event::add('system.routing', array($this, 'route_check'));
+	}
+
+	/**
+	* Check to see if the user can continue to the controller
+	*/
+	public function route_check()
+	{
+		$allowed = FALSE;
+		$allowed_roles = Kohana::config('routes.'.Router::$current_route.'.allowed_roles');
+		if($allowed_roles === NULL)
+		{
+			//everyone is allowed to access this route
+			$allowed = TRUE;
+		}
+		if(!$allowed)
+		{
+			foreach($allowed_roles as $role)
+			{
+				if (in_array($role, $this->user_roles))
+				{
+					$allowed = TRUE;
+				}
+			}
+			if (!$allowed)
+			{
+				Event::run('system.403');
+			}
+		}
+	}
+}
+
+new simple_acl_hook;
